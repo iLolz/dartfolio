51f9d82d754c8296295a639294ccebe6f9d3946e
Marcio Rodrigo de Paula Nascimento <informatica.mrn@gmail.com>
2022-09-08 20:58:57 -0300

    Update dartfolio.dart

--- bin/dartfolio.dart
+++ bin/dartfolio.dart
@@ -3,7 +3,7 @@ import 'package:dartfolio/dartfolio.dart' as dartfolio;
 import 'dart:io';
 
 int calculate() {
-  print("Alencar A. Moreira");
+  print("Marcio R P Nascimento");
   int a = int.parse(stdin.readLineSync(encoding: utf8)!);
   int b = int.parse(stdin.readLineSync(encoding: utf8)!);
 
